3aff55ec56caee9c244700ae2d86b1cce2e71668
Kayanja2023 <KayanjaLwanga@outlook.com>
2026-01-11 00:00:32 +0200

    chore: Fix build paths in angular.json for deployment

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static('./dist/portfolio'));
 
 // Send all requests to index.html
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '/dist/portfolio/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/portfolio/index.html'));
 });
 
 // Start the app by listening on the default port
